b18488c54e33ce49cc59d086565f48039b368172
Takuya <greenflash808808@gmail.com>
2024-12-01 22:43:02 +0900

    サイト全体にrevalidateを設定する

--- app/news/page.tsx
+++ app/news/page.tsx
@@ -4,8 +4,6 @@ import Pagination from "../_conponents/Pagination";
 import SearchField from "../_conponents/SearchField";
 import { NEWS_LIST_LIMIT } from "@/app/_constants";
 
-export const revalidate = 60;
-
 export default async function Page() {
   const { contents: news, totalCount } = await getNewsList({
     limit: NEWS_LIST_LIMIT,
--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,8 @@ import { TOP_NEWS_LIMIT } from "@/app/_constants";
 import NewsList from "@/app/_conponents/NewsList";
 import ButtonLink from "@/app/_conponents/ButtonLink";
 
+export const revalidate = 60;
+
 export default async function home() {
   const data = await getNewsList({
     limit: TOP_NEWS_LIMIT,
